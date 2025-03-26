bbb15541e05264056b351b5e17aea4f7d7579b5b
otavioms <otaviioms@gmail.com>
2025-03-26 17:13:06 -0300

    Ajustando versões para integração com arcore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "br.com.otavioms.shop_ar"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
